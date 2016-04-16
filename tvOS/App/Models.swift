e19d74926305c081cd034967187f423b64a7cc8b
Sami Suteria <sami.suteria@gmail.com>
2016-04-16 12:51:26 -0700

    add track and user models

---
+++ tvOS/App/Models.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct Track {
+    let title: String
+    let artist: String
+    let soundcloudID: String
+    let user: User
+}
+
+struct User {
+    let name: String
+    let emoji: String
+}
