335fdf2363d391d1444cb54d8d53d06bd688fa7f
Sami Suteria <sami.suteria@gmail.com>
2016-04-16 08:55:52 -0700

    New tvOS Project

---
+++ App/App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Sami Suteria on 4/16/16.
+//  Copyright © 2016 CGRekt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.redColor().colorWithAlphaComponent(0.25)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
