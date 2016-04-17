9564ff4d0ca81e968902d967f722b9cf85ab4997
Sami Suteria <sami.suteria@gmail.com>
2016-04-17 01:23:30 -0700

    search function

--- iOS/App/SoundCloudClient.swift
+++ iOS/App/SoundCloudClient.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Alamofire
 
 class SoundCloudClient {
     
@@ -9,23 +10,60 @@ class SoundCloudClient {
         clientSecret: "e1accb5ad7f54ffd70d30b715a39e19a"
     )
     
-    func search(title: String, artist: String) {
+    func search(query: String, completion: (tracks: [SoundCloudTrackModel]?) -> ()) {
         
-        let url = NSURL(string:"https://api.soundcloud.com/tracks?client_id=\(SoundCloud.clientID)")!
-        let dataTask = NSURLSession.sharedSession().dataTaskWithRequest(NSURLRequest(URL: url)) { (data: NSData?, response: NSURLResponse?, error: NSError?) in
+        let encodedQuery = query.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        
+        let url = "https://api.soundcloud.com/tracks?client_id=\(SoundCloud.clientID)&q=\(encodedQuery)"
+        
+        Alamofire.request(.GET, url)
+            .response { (request: NSURLRequest?, response: NSHTTPURLResponse?, data: NSData?, error: NSError?) in
+                
+                guard
+                    let data = data,
+                    let json = try? NSJSONSerialization.JSONObjectWithData(data, options: []),
+                    let tracksData = json as? [[String: AnyObject]]
+                    else {
+                        print("boo")
+                        completion(tracks: nil)
+                        return
+                }
+                
+                print(tracksData.count)
+                
+                var tracks = [SoundCloudTrackModel]()
+                
+                tracksData.forEach({ (trackData: [String : AnyObject]) in
+                    
+                    if
+                        let artworkURLString = trackData["artwork_url"] as? String,
+                        let artworkURL = NSURL(string: artworkURLString),
+                        let id = trackData["id"] as? Int,
+                        let tags = trackData["tag_list"] as? String,
+                        let title = trackData["title"] as? String,
+                        let user = trackData["user"] as? [String: AnyObject],
+                        let username = user["username"] as? String,
+                        let streamable = trackData["streamable"] as? Bool where streamable == true
+                    {
+                        let track = SoundCloudTrackModel(artworkURL: artworkURL,
+                            id: String(id),
+                            tags: tags,
+                            title: title,
+                            username: username)
+                        tracks.append(track)
+                    }
+                })
+                
+                
+                
+                
             
-            guard let data = data,
-                  let json = try? NSJSONSerialization.JSONObjectWithData(data, options: [])
-            else {
-                print("can't serialize data")
-                return
-            }
-            
-            print(json)
-            
-            print("AsdasD")
+                completion(tracks: tracks)
         }
-        dataTask.resume()
+        
+        
+        
+        
         
     }
     
